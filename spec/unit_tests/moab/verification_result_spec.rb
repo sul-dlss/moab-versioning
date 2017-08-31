b447c312d048bfcaa3da08d1f6cf52751398a440
Naomi Dushay <ndushay@stanford.edu>
2017-08-30 19:01:43 -0700

    unit_tests/moab/verification_result_spec: remove cruft (inspired by rubocop)

--- spec/unit_tests/moab/verification_result_spec.rb
+++ spec/unit_tests/moab/verification_result_spec.rb
@@ -1,122 +1,104 @@
 require 'spec_helper'
 
-
-
-# Unit tests for class {Moab::StorageObjectVersion}
 describe 'Moab::VerificationResult' do
 
-  describe '=========================== CONSTRUCTOR ===========================' do
-
-    specify 'Moab::VerificationResult#initialize' do
-      result = Moab::VerificationResult.new('my_entity')
-      expect(result.entity).to eq('my_entity')
-      expect(result.verified).to eq(false)
-      expect(result.details).to eq(nil)
-      expect(result.subentities).to be_an_instance_of(Array)
-    end
-
+  specify '.verify_value' do
+    result = Moab::VerificationResult.verify_value('greeting', "hello", "goodbye")
+    expect(result.entity).to eq 'greeting'
+    expect(result.verified).to eq false
+    expect(result.details).to eq({ "expected"=>"hello", "found"=>"goodbye" })
+    expect(result.subentities).to be_an_instance_of Array
+    expect(result.subentities.size).to eq 0
   end
 
-  describe '========================= CLASS METHODS ==========================' do
-
-    specify 'Moab::VerificationResult.verify_value' do
-      result = Moab::VerificationResult.verify_value('greeting',"hello","goodbye")
-      expect(result.entity).to eq('greeting')
-      expect(result.verified).to eq(false)
-      expect(result.details).to eq({"expected"=>"hello", "found"=>"goodbye"})
-      expect(result.subentities).to be_an_instance_of(Array)
-      expect(result.subentities.size).to eq(0)
-    end
-
-    specify 'Moab::VerificationResult.verify_truth' do
-      result = Moab::VerificationResult.verify_truth('truth',"true")
-      expect(result.entity).to eq('truth')
-      expect(result.verified).to eq(true)
-      expect(result.details).to eq(nil)
-      expect(result.subentities).to be_an_instance_of(Array)
-      expect(result.subentities.size).to eq(0)
-    end
-
+  specify '.verify_truth' do
+    result = Moab::VerificationResult.verify_truth('truth', "true")
+    expect(result.entity).to eq 'truth'
+    expect(result.verified).to eq true
+    expect(result.details).to eq nil
+    expect(result.subentities).to be_an_instance_of Array
+    expect(result.subentities.size).to eq 0
   end
 
-  describe '======================= INSTANCE METHODS ==========================' do
+  specify '#initialize' do
+    result = Moab::VerificationResult.new('my_entity')
+    expect(result.entity).to eq 'my_entity'
+    expect(result.verified).to eq false
+    expect(result.details).to eq nil
+    expect(result.subentities).to be_an_instance_of Array
+  end
 
-    specify 'Moab::VerificationResult.to_hash' do
-      result = Moab::VerificationResult.new('my_entity')
-      result.verified = false
-      result.subentities << Moab::VerificationResult.new('subentity_1')
-      result.subentities << Moab::VerificationResult.new('subentity_2')
-      result.subentities << Moab::VerificationResult.new('subentity_3')
-      result.subentities.each do |s|
-        if (s.entity == 'subentity_2')
-          s.verified = false
-          s.details = {"its a" => "shame"}
-        else
-          s.verified = true
-          s.details = {"all is" => "good"}
-        end
+  let(:result) do
+    result = Moab::VerificationResult.new('my_entity')
+    result.verified = false
+    result.subentities << Moab::VerificationResult.new('subentity_1')
+    result.subentities << Moab::VerificationResult.new('subentity_2')
+    result.subentities << Moab::VerificationResult.new('subentity_3')
+    result.subentities.each do |s|
+      if (s.entity == 'subentity_2')
+        s.verified = false
+        s.details = {"its a" => "shame"}
+      else
+        s.verified = true
+        s.details = {"all is" => "good"}
       end
-      detail_hash = result.to_hash(verbose=true)
-      #puts JSON.pretty_generate(detail_hash)
-      expect("#{JSON.pretty_generate(detail_hash)}\n").to eq <<-EOF
-{
-  "my_entity": {
-    "verified": false,
-    "details": {
-      "subentity_1": {
-        "verified": true,
-        "details": {
-          "all is": "good"
-        }
-      },
-      "subentity_2": {
-        "verified": false,
-        "details": {
-          "its a": "shame"
-        }
-      },
-      "subentity_3": {
-        "verified": true,
-        "details": {
-          "all is": "good"
-        }
-      }
-    }
-  }
-}
-      EOF
-
-      detail_json = result.to_json(verbose=false)
-      #puts detail_json
-      expect("#{detail_json}\n").to eq <<-EOF
-{
-  "my_entity": {
-    "verified": false,
-    "details": {
-      "subentity_1": {
-        "verified": true
-      },
-      "subentity_2": {
-        "verified": false,
-        "details": {
-          "its a": "shame"
-        }
-      },
-      "subentity_3": {
-        "verified": true
-      }
-    }
-  }
-}
-      EOF
-
-
     end
-
+    result
   end
 
+  specify '#to_hash' do
+    detail_hash = result.to_hash(true)
+    expect("#{JSON.pretty_generate(detail_hash)}\n").to eq <<-EOF
+{
+  "my_entity": {
+    "verified": false,
+    "details": {
+      "subentity_1": {
+        "verified": true,
+        "details": {
+          "all is": "good"
+        }
+      },
+      "subentity_2": {
+        "verified": false,
+        "details": {
+          "its a": "shame"
+        }
+      },
+      "subentity_3": {
+        "verified": true,
+        "details": {
+          "all is": "good"
+        }
+      }
+    }
+  }
+}
+EOF
+  end
 
-
+  it '#to_json' do
+    detail_json = result.to_json(false)
+    expect("#{detail_json}\n").to eq <<-EOF
+{
+  "my_entity": {
+    "verified": false,
+    "details": {
+      "subentity_1": {
+        "verified": true
+      },
+      "subentity_2": {
+        "verified": false,
+        "details": {
+          "its a": "shame"
+        }
+      },
+      "subentity_3": {
+        "verified": true
+      }
+    }
+  }
+}
+EOF
+  end
 end
-
-
