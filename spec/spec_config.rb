1ee6ac3e2af72247006c5773c111f50e10c848e4
Naomi Dushay <ndushay@stanford.edu>
2017-09-01 00:10:02 -0700

    spec_config: refactor storage_roots for readability

--- spec/spec_config.rb
+++ spec/spec_config.rb
@@ -1,5 +1,7 @@
 Moab::Config.configure do
-  storage_roots [File.join(File.dirname(__FILE__),"fixtures/derivatives"),File.join(File.dirname(__FILE__),"fixtures/newnode")]
+  root_path1 = File.join(File.dirname(__FILE__), "fixtures/derivatives")
+  root_path2 = File.join(File.dirname(__FILE__), "fixtures/newnode")
+  storage_roots [root_path1, root_path2]
   storage_trunk 'ingests'
   deposit_trunk 'packages'
   path_method :druid
