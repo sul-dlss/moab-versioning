69d60d78b6d3b1dd7e45aa636de1de4c71968a1f
Naomi Dushay <ndushay@stanford.edu>
2017-09-09 02:56:27 -0700

    unit_tests/moab/file_manifestation_spec: refactor final cleanup

--- spec/unit_tests/moab/file_manifestation_spec.rb
+++ spec/unit_tests/moab/file_manifestation_spec.rb
@@ -1,6 +1,21 @@
 require 'spec_helper'
 
 describe 'Moab::FileManifestation' do
+  let(:file_manifestation) do
+    v1_base_directory = @fixtures.join('data/jq937jp0017/v0001/content')
+    title_v1_pathname = @fixtures.join('data/jq937jp0017/v0001/content/title.jpg')
+    page1_v1_pathname = @fixtures.join('data/jq937jp0017/v0001/content/page-1.jpg')
+
+    title_v1_signature = Moab::FileSignature.new.signature_from_file(title_v1_pathname)
+    title_v1_instance = Moab::FileInstance.new.instance_from_file(title_v1_pathname, v1_base_directory)
+    page1_v1_instance = Moab::FileInstance.new.instance_from_file(page1_v1_pathname, v1_base_directory)
+
+    fm = Moab::FileManifestation.new
+    fm.signature = title_v1_signature
+    fm.instances << title_v1_instance
+    fm.instances << page1_v1_instance
+    fm
+  end
 
   describe '#initialize' do
     specify 'empty options hash' do
@@ -19,7 +34,6 @@ describe 'Moab::FileManifestation' do
   end
 
   describe '#signature' do
-    let(:file_manifestation) { Moab::FileManifestation.new }
     let(:value) { double(Moab::FileSignature.name) }
 
     specify 'single value' do
@@ -30,22 +44,6 @@ describe 'Moab::FileManifestation' do
       file_manifestation.signature = [value]
       expect(file_manifestation.signature).to eq value
     end
-  end
-
-  let(:file_manifestation) do
-    v1_base_directory = @fixtures.join('data/jq937jp0017/v0001/content')
-    title_v1_pathname = @fixtures.join('data/jq937jp0017/v0001/content/title.jpg')
-    page1_v1_pathname = @fixtures.join('data/jq937jp0017/v0001/content/page-1.jpg')
-
-    title_v1_signature = Moab::FileSignature.new.signature_from_file(title_v1_pathname)
-    title_v1_instance = Moab::FileInstance.new.instance_from_file(title_v1_pathname, v1_base_directory)
-    page1_v1_instance = Moab::FileInstance.new.instance_from_file(page1_v1_pathname, v1_base_directory)
-
-    fm = Moab::FileManifestation.new
-    fm.signature = title_v1_signature
-    fm.instances << title_v1_instance
-    fm.instances << page1_v1_instance
-    fm
   end
 
   specify '#paths' do
