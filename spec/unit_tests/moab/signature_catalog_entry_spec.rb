7b67d9a65d94d3e717ccd90f45b614ad57c08d67
Naomi Dushay <ndushay@stanford.edu>
2017-09-09 01:24:20 -0700

    unit_tests/moab/signature_catalog_entry_spec: refactor 'instance method' tests

--- spec/unit_tests/moab/signature_catalog_entry_spec.rb
+++ spec/unit_tests/moab/signature_catalog_entry_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe 'Moab::SignatureCatalogEntry' do
 
+  let(:sce) { Moab::SignatureCatalogEntry.new() }
+
   specify '#initialize' do
     opts = {
       version_id: 26,
@@ -17,35 +19,15 @@ describe 'Moab::SignatureCatalogEntry' do
   end
 
   specify '#signature becomes first value if set to Array' do
-    sce = Moab::SignatureCatalogEntry.new()
     value = double(Moab::FileSignature.name)
     sce.signature = [value, 'dummy']
     expect(sce.signature).to eq value
   end
 
-  describe '=========================== INSTANCE METHODS ===========================' do
-
-    before(:each) do
-      @opts = {}
-      @signature_catalog_entry = Moab::SignatureCatalogEntry.new(@opts)
-
-      @signature_catalog_entry.version_id = 5
-      @signature_catalog_entry.group_id = 'content'
-      @signature_catalog_entry.path = 'title.jpg'
-      @signature_catalog_entry.signature = double(Moab::FileSignature.name)
-    end
-
-    # Unit test for method: {Moab::SignatureCatalogEntry#storage_path}
-    # Which returns: [String] Returns the storage path to a file, relative to the object storage home directory
-    # For input parameters: (None)
-    specify 'Moab::SignatureCatalogEntry#storage_path' do
-      expect(@signature_catalog_entry.storage_path()).to eq("v0005/data/content/title.jpg")
-
-      # def storage_path
-      #   File.join(Moab::StorageObject.version_dirname(version_id),'data', group_id, path)
-      # end
-    end
-
+  specify '#storage_path' do
+    sce.version_id = 5
+    sce.group_id = 'content'
+    sce.path = 'title.jpg'
+    expect(sce.storage_path()).to eq("v0005/data/content/title.jpg")
   end
-
 end
