3eb6ea6689fb9460544ce3e863bb9b6e7e1df848
Naomi Dushay <ndushay@stanford.edu>
2017-09-09 21:47:42 -0700

    unit_tests/moab/file_group_diff_subset_spec: refactor attribute/method specs

--- spec/unit_tests/moab/file_group_difference_subset_spec.rb
+++ spec/unit_tests/moab/file_group_difference_subset_spec.rb
@@ -16,41 +16,15 @@ describe 'Moab::FileGroupDifferenceSubset' do
     end
   end
 
-  describe '=========================== INSTANCE ATTRIBUTES ===========================' do
+  let(:diff_subset) { Moab::FileGroupDifferenceSubset.new }
 
-    before(:all) do
-      opts = {}
-      @file_group_difference_subset = Moab::FileGroupDifferenceSubset.new(opts)
-    end
-
-    # Unit test for attribute: {Moab::FileGroupDifferenceSubset#change}
-    # Which stores: [String] The type of change (identical|renamed|modified|deleted|added)
-    specify 'Moab::FileGroupDifferenceSubset#change' do
-      value = 'Test change'
-      @file_group_difference_subset.change= value
-      expect(@file_group_difference_subset.change).to eq(value)
-
-      # attribute :change, String, :key => true
-    end
-
-    # Unit test for attribute: {Moab::FileGroupDifferenceSubset#count}
-    # Which stores: [Integer] How many files were changed
-    specify 'Moab::FileGroupDifferenceSubset#count' do
-      value = 52
-      @file_group_difference_subset.count= value
-      expect(@file_group_difference_subset.count).to eq(0)
-
-      # attribute :count, Integer, :on_save => Proc.new { |n| n.to_s }
-    end
-
-    # Unit test for attribute: {Moab::FileGroupDifferenceSubset#files}
-    # Which stores: [Array<Moab::FileInstanceDifference>] The set of file instances having this type of change
-    specify 'Moab::FileGroupDifferenceSubset#files' do
-      expect(@file_group_difference_subset.files.size).to eq(0)
-
-      # has_many :files, Moab::FileInstanceDifference
-    end
-
+  specify '#count is computed, not set' do
+    value = 52
+    diff_subset.count = value
+    expect(diff_subset.count).to eq 0
   end
 
+  specify '#files is computed, not set' do
+    expect(diff_subset.files.size).to eq 0
+  end
 end
