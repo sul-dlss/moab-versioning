d7b8c09743fd8c3cea37384a5c4f13ef7c64d63b
Johnathan Martin <john.martin@stanford.edu>
2017-10-27 02:13:45 -0700

    a bit of refactoring

--- lib/moab/storage_object_validator.rb
+++ lib/moab/storage_object_validator.rb
@@ -7,6 +7,9 @@ module Moab
 
     EXPECTED_VERSION_SUB_DIRS = ["data", "manifests"].freeze
     EXPECTED_DATA_SUB_DIRS = ["content", "metadata"].freeze
+    IMPLICIT_DIRS = ['.', '..'].freeze # unlike Find.find, Dir.entries returns these
+
+    # error codes
     INCORRECT_DIR = 0
     MISSING_DIR = 1
     EXTRA_DIR_DETECTED = 2
@@ -15,51 +18,69 @@ module Moab
     NO_MANIFEST_INVENTORY = 6
     NO_XML_FILES = 7
     VERSIONS_NOT_IN_ORDER = 9
-    EXTRA_FILE_IN_VERSION_DIR = 10
+    FILES_IN_VERSION_DIR = 10
 
     RESPONSE_CODE_TO_MESSAGES = {
       INCORRECT_DIR=> "Incorrect items in path",
       MISSING_DIR => "Missing directory: %{addl}",
       EXTRA_DIR_DETECTED => "Unexpected item in path: %{addl}",
       EMPTY => "No items in path",
-      EXTRA_FILE_IN_VERSION_DIR => "extra file in versino dir",
+      FILES_IN_VERSION_DIR => "Should contain only sequential version directories. Also contains files: %{addl}",
       NO_SIGNATURE_CATALOG => "Version: %{addl} Missing signatureCatalog.xml",
       NO_MANIFEST_INVENTORY => "Version: %{addl} Missing manifestInventory.xml",
       NO_XML_FILES => "Version: %{addl} Missing all required metadata files",
-      VERSIONS_NOT_IN_ORDER => "Version directories are not in order. Here are the current versions: %{addl}"
+      VERSIONS_NOT_IN_ORDER => "Should contain only sequential version directories. Current directories: %{addl}"
     }.freeze
 
-    attr_reader :path
+    attr_reader :storage_obj_path
 
     def initialize(storage_object)
-      @path = storage_object.object_pathname
+      @storage_obj_path = storage_object.object_pathname
+      @directory_entries_hash = {}
     end
 
-    def validate_object
-      results = []
-      if sequential_version.empty?
-        p "yay in order"
-        version_directories = sub_dirs(path).sort 
-        version_directories.each do |version|
-          # return false if Dir["#{path}/#{version}"].empty?
-          version_path = "#{@path}/#{version}"
-          version_sub_dirs = sub_dirs(version_path).sort
-          p check_version_sub_dirs(version_sub_dirs, version)
-        end
-      else 
-        p results
-      end
-    end
+    # def validate_object
+    #   results = []
+    #   if check_for_only_sequential_version_dirs.empty?
+    #     p "yay in order"
+    #     version_directories = sub_dirs(storage_obj_path).sort 
+    #     version_directories.each do |version|
+    #       # return false if Dir["#{storage_obj_path}/#{version}"].empty?
+    #       version_path = "#{storage_obj_path}/#{version}"
+    #       version_sub_dirs = sub_dirs(version_path).sort
+    #       p check_version_sub_dirs(version_sub_dirs, version)
+    #     end
+    #   else 
+    #     p results
+    #   end
+    # end
 
 
     # def validate_object
     #   results = []
     #   # results.concat check_manifest_dir_xml
-    #   results.concat sequential_version
-    #   # results.concat no_nested_moabs
+    #   results.concat check_for_only_sequential_version_dirs
+    #   # results.concat check_no_nested_moabs
     # end
 
 
+    def validate_object
+      results = []
+
+      results.concat check_for_only_sequential_version_dirs
+
+      # proceed if we only have sequential version directories uder the root object path
+      if results.size == 0
+        version_directories = sub_dirs(storage_obj_path)
+        version_directories.each do |version_dir|
+          version_path = "#{storage_obj_path}/#{version_dir}"
+          version_sub_dirs = sub_dirs(version_path)
+          results.concat check_version_sub_dirs(version_sub_dirs, version_dir)
+        end
+      end
+
+      results
+    end
 
 
     # private
@@ -67,33 +88,45 @@ module Moab
 
     def check_manifest_dir_xml
       results = []
-      version_directories = sub_dirs(@path).sort # sort for travis
+      version_directories = sub_dirs(storage_obj_path).sort # sort for travis
       version_directories.each do |version|
-        version_path = "#{@path}/#{version}"
+        version_path = "#{storage_obj_path}/#{version}"
         results.concat check_manifest_files(version_path, version)
       end
       results
     end
     
-    def sequential_version
+    def check_for_only_sequential_version_dirs
       results = []
 
-      ver_dir = sub_dirs(path).sort # sort for travis
-      ver_dir_num = ver_dir.collect { |ver| ver[-2..-1].to_i }
-      results.concat sequential_order(ver_dir_num)
+      # ver_dir = sub_dirs(storage_obj_path).sort # sort for travis
+      # ver_dir_num = ver_dir.collect { |ver| ver[-2..-1].to_i }
+      # results.concat sequential_order(ver_dir_num)
 
-      if Dir.entries(path).detect { |entry| FileTest.file?(entry) }
-        results << result_hash(EXTRA_FILE_IN_VERSION_DIR)
+      sub_dirs(storage_obj_path).each_with_index do |dir_name, index|
+        expected_vers_num = index + 1 # version numbering starts at 1, array indexing starts at 0
+        begin
+          if dir_name[1..-1].to_i != expected_vers_num
+            results << result_hash(VERSIONS_NOT_IN_ORDER, sub_dirs(storage_obj_path))
+            break
+          end
+        rescue ArgumentError
+          results << result_hash(VERSIONS_NOT_IN_ORDER, sub_dirs(storage_obj_path))
+        end
+      end
+
+      if files_in_dir(storage_obj_path).size > 0
+        results << result_hash(FILES_IN_VERSION_DIR, files_in_dir(storage_obj_path))
       end
 
       results
     end
 
-    def no_nested_moabs
+    def check_no_nested_moabs
       results = []
-      version_directories = sub_dirs(@path).sort # sort for travis
+      version_directories = sub_dirs(storage_obj_path).sort # sort for travis
       version_directories.each do |version|
-        version_path = "#{@path}/#{version}"
+        version_path = "#{storage_obj_path}/#{version}"
         version_sub_dirs = sub_dirs(version_path).sort
         results.concat check_version_sub_dirs(version_sub_dirs, version)
         data_dir_path = "#{version_path}/#{version_sub_dirs[0]}"
@@ -133,8 +166,27 @@ module Moab
       results
     end
 
+    def directory_entries(path)
+      @directory_entries_hash[path] ||=
+        begin
+          ret_val = { files: [], dirs: [] }
+          (Dir.entries(path).sort - IMPLICIT_DIRS).each do |child|
+            if FileTest.file? child
+              ret_val[:files] << child
+            else
+              ret_val[:dirs] << child
+            end
+          end
+          ret_val
+        end
+    end
+
     def sub_dirs(path)
-      Dir.entries(path).select { |entry| File.join(path, entry) unless /^\..*/ =~ entry }
+      directory_entries(path)[:dirs]
+    end
+
+    def files_in_dir(path)
+      directory_entries(path)[:files]
     end
 
     def found_unexpected(array, version, dir=nil) #maybe just add required_sub_dirs = EXPECTED_DATA_SUB_DIRS as a parameter
@@ -187,19 +239,19 @@ module Moab
       results
     end
 
-    def sequential_order(array)
-      results = []
-      sorted = array.sort
-      lastNum = sorted[0]
-      sorted[1, sorted.count].each do |n|
-        if lastNum + 1 != n
-          results << result_hash(VERSIONS_NOT_IN_ORDER, sorted) 
-          break
-        end
-        lastNum = n
-      end
-      results
-    end
+    # def sequential_order(array)
+    #   results = []
+    #   sorted = array.sort
+    #   lastNum = sorted[0]
+    #   sorted[1, sorted.count].each do |n|
+    #     if lastNum + 1 != n
+    #       results << result_hash(VERSIONS_NOT_IN_ORDER, sorted) 
+    #       break
+    #     end
+    #     lastNum = n
+    #   end
+    #   results
+    # end
 
     def only_expected_directories?(dir_path)
       # files = Dir.entries(dir_path).select { |f| File.file?(dir_path+f) unless /^\..*/ =~ f }
